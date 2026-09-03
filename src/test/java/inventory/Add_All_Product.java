dcc3ad8822127731b1df0f8ced03ba14d527359a
yadav-AshutoshG <y.ashutoshg@gmail.com>
2026-09-03 18:30:04 +0530

    Inventory Module Click

--- src/test/java/inventory/Add_All_Product.java
+++ src/test/java/inventory/Add_All_Product.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Add_All_Product {
 
@@ -13,13 +16,15 @@ public class Add_All_Product {
 		// TODO Auto-generated method stub
 
 		
-		
+		// open Browser
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		
 	     driver.get("https://imsmymunc.com/");
-		
+		 /*
+		  * Login 
+		  */
 	     
 	     driver.findElement(By.xpath("//a[@href='/all-login']")).click();
 	     Thread.sleep(1000);
@@ -37,8 +42,79 @@ public class Add_All_Product {
 	     
 	     driver.findElement(By.xpath("//div[@class='form-login\']")).click();
 
-//	     fjgldfjgjfdl;jg;ldfj;gh;dfjgh
 	     
+	     
+//all  product
+	     
+	     
+	     
+//	     
+//	     By inventoryMenu = By.xpath("//span[normalize-space()='Inventory']");
+//
+//	     By allProducts = By.xpath(
+//	         "//a[@href='/product' and normalize-space()='All Products']"
+//	     );
+//
+//	     public InventoryPage(WebDriver driver) {
+//	         this.driver = driver;
+//	     }
+//
+//	     public void clickInventory() {
+//	         driver.findElement(inventoryMenu).click();
+//	     }
+//
+//	     public void clickAllProducts() {
+//	         driver.findElement(allProducts).click();
+//	     
+	     
+	     /*
+	         * Inventory → All Products
+	         */
+
+	     
+	     
+	     
+	     
+	     
+	        // Click Inventory
+	        driver.findElement(By.xpath(
+	                "//span[normalize-space()='Inventory']"
+	        )).click();
+
+	        // Click All Products
+	        driver.findElement(By.xpath(
+	                "//a[@href='/product' and normalize-space()='All Products']"
+	        )).click();
+
+//	     Add Product 
+//	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//	     // Click All Products
+//	        wait.until(ExpectedConditions.elementToBeClickable(
+//	                By.xpath("//a[@href='/product' and normalize-space()='All Products']")
+//	        )).click();
+//
+//
+//	        // Wait for Add Products
+//	        By addProducts = By.xpath("//div[contains(@class,'Add-Button')]");
+//
+//	        wait.until(ExpectedConditions.elementToBeClickable(addProducts)).click();
+//	    
+//	     
+	        driver.findElement(By.xpath("//div[text()='Add Products']")).click();
+	        
+	        
+	        
+	        
+	     
+	     
+	     
+	     /*
+	      * Logout
+	      */
+	     
+	     
+	     
+//	     Close Browser
 	     Thread.sleep(9000);
 	     driver.close();
 	     driver.quit();
